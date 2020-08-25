8ed8df4051f595af60400066118b08ec4bb4a8e3
scottvonscott <skeplin17@gmail.com>
2020-08-25 11:19:06 -0700

    Seed Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MainCourse.create(name: "Pizza", notes: "Extra Cheesey")
+Snack.create(name: "Nachos", notes: "Extra Cheesey")
+Drink.create(name: "Soda Pop", notes: "No Coke, Pepsi")
+Party.create(title: "Pizza Party")
+Plan.create(party_id: 1, main_course_id: 1, snack_id: 1, drink_id: 1)
