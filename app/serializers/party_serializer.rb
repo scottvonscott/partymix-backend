422126dad571c1e49ac5f186422ed2e383322771
scottvonscott <skeplin17@gmail.com>
2020-08-27 16:12:45 -0700

    custom party serializer

--- app/serializers/party_serializer.rb
+++ app/serializers/party_serializer.rb
@@ -1,9 +1,11 @@
 class PartySerializer
   include FastJsonapi::ObjectSerializer
   attributes :title, :id
-  attribute :items do |object|
-    byebug
-    "#{object.items} (#{object.items.category.name})"
+  attribute :items_and_categories do |object|
+    object.items.map do |c|
+      {name: c.name, id: c.id, category: c.category.name, category_id: c.category.id}
+    end
+     
   end
 
 end
